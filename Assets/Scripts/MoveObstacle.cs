9aa4755078c6f29e08457de80a36e9b23f358103
wlsdkrla <wlswlsdk23@naver.com>
2023-03-09 02:39:51 +0900

    장애물 코드 수정(UpDown, LeftRight, Drop, Rotate)

--- Assets/Scripts/MoveObstacle.cs
+++ Assets/Scripts/MoveObstacle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MoveObstacle : MonoBehaviour
 {
+    public enum MoveObstacleType { A,B,C,D,E,F};
+    public MoveObstacleType Type;
+    //PlayerController player;
     //UD_Floor
     float initPositionY;
     float initPositionX;
@@ -14,20 +17,39 @@ public class MoveObstacle : MonoBehaviour
     public float moveSpeed;
 
     //RT_Floor
-    //public float rotateSpeed;
+    public float rotateSpeed;
 
+    //Big Jump
+    public bool isBigJump;
+    //Drop
+    public float dropSpeed;
+    //Swing
+    public float initRotationZ;
+    public float swingPoint;
+    float z;
     void Awake()
     {
-        if (gameObject.name == "Modeling_3-2(updown)")
+        if (Type == MoveObstacleType.A) // Up & Down
         {
             initPositionY = transform.position.y;
             turningPoint = initPositionY - distance;
         }
-        //if (gameObject.name == "LR_Floor")
-        //{
-        //    initPositionX = transform.position.x;
-        //    turningPoint = initPositionX - distance;
-        //}
+        if(Type == MoveObstacleType.B) // Right & Left
+        {
+            initPositionX = transform.position.x;
+            turningPoint = initPositionX - distance;
+        }
+        /*if (Type == MoveObstacleType.F)
+        {
+            // -180 ~ 0
+            initRotationZ = transform.rotation.z; // 현재 z값 == -90
+            swingPoint = initRotationZ - distance; // 스윙 포인트는 현재 z값에서 distance 더한 값
+        }*/
+        // Case C == Rotate
+        // Case D == Big Jump
+
+        // Case E == Delay & Drop
+        // Case F == Swing
     }
 
     void upDown()
@@ -52,10 +74,10 @@ public class MoveObstacle : MonoBehaviour
             transform.position = transform.position + new Vector3(0, -1, 0) * moveSpeed * Time.deltaTime;
         }
     }
-    //void rotate()
-    //{
-    //    transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
-    //}
+    void rotate()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
     void leftRight()
     {
 
@@ -80,21 +102,49 @@ public class MoveObstacle : MonoBehaviour
         }
 
     }
+    void OnTriggerEnter(Collider other) // Case E == Delay & Drop
+    {
+        if(other.gameObject.tag == "Player" )
+        {
+            
+            transform.position = Vector3.Lerp(transform.position, other.transform.position, dropSpeed);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player" && isBigJump)
+        {
+            collision.rigidbody.AddForce(Vector3.up * 30, ForceMode.Impulse);
+           
+            isBigJump = false;
+        }
 
+    }
+    void Swing()
+    {
+
+
+    }
     void Update()
     {
-        if (gameObject.name == "Modeling_3-2(updown)")
+        switch (Type)
         {
-            upDown();
+            case MoveObstacleType.A:
+                upDown();
+                break;
+            case MoveObstacleType.B:
+                leftRight();
+                break;
+            case MoveObstacleType.C:
+                rotate();
+                break;
+            case MoveObstacleType.D:
+                isBigJump = true;
+                break;
+            case MoveObstacleType.F:
+                Swing();
+                break;
         }
-        //if (gameObject.name == "RT_Floor")
-        //{
-        //    rotate();
-        //}
-        //if (gameObject.name == "LR_Floor")
-        //{
-        //    leftRight();
-        //}
-
+       
     }
 }
